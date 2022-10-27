62248c514c525218b6de4336a44da083df3308b7
Brittany McGarr <brittanymcgarr@brittanys-mbp.lan>
2022-10-26 21:19:34 -0700

    PUSH-03 configuring cli

--- lib/R2/cli.rb
+++ lib/R2/cli.rb
@@ -3,8 +3,8 @@ require 'R2'
 
 class CLI < Thor
 
-  desc "get AWS_ENV", "Determines the current set of AWS environment variables"
-  def get_aws_env
+  desc "configure AWS_ENV", "Determines the current set of AWS environment variables"
+  def configure_aws
     puts R2::R2.configure_aws
   end
 
